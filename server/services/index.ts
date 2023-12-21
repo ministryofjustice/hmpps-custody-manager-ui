59f4c610d832a4b636f5f24601507a6829f1d5bb
David Winchurch <david.winchurch@digital.justice.gov.uk>
2023-12-21 08:38:46 +0000

    CMG-5: API types and frontend components (#3)
    * CMG-5: Add required package json scripts
    
    * CMG-5: Application name
    
    * CMG-5: API client types
    
    * CMG-5: Add frontend components
    
    * CMG-5: Update secrets

--- server/services/index.ts
+++ server/services/index.ts
@@ -1,14 +1,18 @@
 import { dataAccess } from '../data'
 import UserService from './userService'
+import FeComponentsService from './feComponentsService'
 
 export const services = () => {
-  const { applicationInfo, manageUsersApiClient } = dataAccess()
+  const { applicationInfo, manageUsersApiClient, feComponentsClient } = dataAccess()
 
   const userService = new UserService(manageUsersApiClient)
+
+  const feComponentsService = new FeComponentsService(feComponentsClient)
 
   return {
     applicationInfo,
     userService,
+    feComponentsService,
   }
 }
 
