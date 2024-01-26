56c690b614362b522700a5f094f90210efe2fd38
David Winchurch <david.winchurch@digital.justice.gov.uk>
2024-01-26 14:20:17 +0000

    CMG-14: Remove jquery

--- server/middleware/setUpStaticResources.ts
+++ server/middleware/setUpStaticResources.ts
@@ -23,17 +23,12 @@ export default function setUpStaticResources(): Router {
     '/node_modules/@ministryofjustice/frontend',
     '/node_modules/hmpps-design-system-frontend/hmpps/assets',
     '/node_modules/hmpps-design-system-frontend',
-    '/node_modules/jquery/dist',
   ).forEach(dir => {
     router.use('/assets', express.static(path.join(process.cwd(), dir), cacheControl))
   })
 
   Array.of('/node_modules/govuk_frontend_toolkit/images').forEach(dir => {
     router.use('/assets/images/icons', express.static(path.join(process.cwd(), dir), cacheControl))
-  })
-
-  Array.of('/node_modules/jquery/dist/jquery.min.js').forEach(dir => {
-    router.use('/assets/js/jquery.min.js', express.static(path.join(process.cwd(), dir), cacheControl))
   })
 
   // Don't cache dynamic resources
