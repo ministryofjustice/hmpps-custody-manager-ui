8cfb9b0050fc51f62d96e7dc506e22d1dfaf3221
yasin <yasin.mustafa@gmail.com>
2024-02-13 10:01:54 +0000

    CMG-19 Add initial version of next-cort-hearing section on overview page

---
+++ server/routes/prisoner/handlers/overview.test.ts
@@ -0,0 +1,46 @@
+import { Express } from 'express'
+
+import request from 'supertest'
+import PrisonerService from '../../../services/prisonerService'
+import { appWithAllRoutes } from '../../testutils/appSetup'
+import { Prisoner } from '../../../@types/prisonerSearchApi/types'
+import PrisonerSearchService from '../../../services/prisonerSearchService'
+import { CourtEventDetails } from '../../../@types/prisonApi/types'
+
+jest.mock('../../../services/prisonerService')
+jest.mock('../../../services/prisonerSearchService')
+
+const prisonerService = new PrisonerService(null) as jest.Mocked<PrisonerService>
+const prisonerSearchService = new PrisonerSearchService(null) as jest.Mocked<PrisonerSearchService>
+
+let app: Express
+
+beforeEach(() => {
+  app = appWithAllRoutes({
+    services: {
+      prisonerService,
+      prisonerSearchService,
+    },
+  })
+})
+
+afterEach(() => {
+  jest.resetAllMocks()
+})
+
+describe('Route Handlers - Prisoner', () => {
+  describe('GET /', () => {
+    it('should render index page', () => {
+      prisonerSearchService.getByPrisonerNumber.mockResolvedValue({ prisonerNumber: 'A12345B' } as Prisoner)
+      prisonerService.getNextCourtEvent.mockResolvedValue({ caseReference: 'TS0001' } as CourtEventDetails)
+
+      return request(app)
+        .get('/prisoner/A12345B/overview')
+        .expect('Content-Type', /html/)
+        .expect(res => {
+          expect(res.text).toContain('<h3 class="govuk-heading-m">Next court hearing</h3>')
+          expect(res.text).toMatch(/Case reference\s*<\/dt>\s*<dd class="govuk-summary-list__value">\s*TS0001/)
+        })
+    })
+  })
+})
--- server/services/prisonerService.ts
+++ server/services/prisonerService.ts
@@ -1,12 +1,17 @@
 import { Readable } from 'stream'
 import PrisonApiClient from '../data/prisonApiClient'
 import { HmppsAuthClient } from '../data'
+import { CourtEventDetails } from '../@types/prisonApi/types'
 
 export default class PrisonerService {
   constructor(private readonly hmppsAuthClient: HmppsAuthClient) {}
 
   async getPrisonerImage(username: string, prisonerNumber: string): Promise<Readable> {
     return new PrisonApiClient(await this.getSystemClientToken(username)).getPrisonerImage(prisonerNumber)
+  }
+
+  async getNextCourtEvent(bookingId: number, token: string): Promise<CourtEventDetails> {
+    return new PrisonApiClient(token).getNextCourtEvent(bookingId)
   }
 
   private async getSystemClientToken(username: string): Promise<string> {
