84e94be45598ca9d0f313489d2f866cb5d021552
carloveo-moj <144013256+carloveo-moj@users.noreply.github.com>
2024-03-06 13:12:08 +0000

    CMG-54 (#32)
    * CMG-54 remove links from sub nav
    
    * remove document page
    
    * upgrade design library

--- server/routes/prisoner/handlers/documents.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express'
-
-export default class DocumentsRoutes {
-  GET = async (req: Request, res: Response): Promise<void> => {
-    const { prisoner } = req
-
-    res.render('pages/prisoner/documents', { prisoner })
-  }
-}
