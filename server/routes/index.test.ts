637a12ea35920bbd57f33da58eacaa562f8778b8
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2024-03-06 16:18:31 +0000

    redirect test

---
+++ server/routes/index.test.ts
@@ -0,0 +1,10 @@
+import request from 'supertest'
+import { appWithAllRoutes } from './testutils/appSetup'
+import config from '../config'
+
+describe('Route Handlers - Index', () => {
+  it('should redirect to dps', () => {
+    const app = appWithAllRoutes({})
+    return request(app).get('/').expect(302).expect('Location', config.digitalPrisonServicesUrl)
+  })
+})
