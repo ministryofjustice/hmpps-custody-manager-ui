698176f4516eb41b41aa9c29b7c956243a0b260c
carloveo-moj <144013256+carloveo-moj@users.noreply.github.com>
2024-03-08 09:44:21 +0000

    CMG-87 redirect if no access to adjustments (#39)

--- server/routes/prisoner/handlers/overview.ts
+++ server/routes/prisoner/handlers/overview.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import PrisonerService from '../../../services/prisonerService'
 import AdjustmentsService from '../../../services/adjustmentsService'
+import config from '../../../config'
 
 export default class OverviewRoutes {
   constructor(
@@ -11,24 +12,27 @@ export default class OverviewRoutes {
   GET = async (req: Request, res: Response): Promise<void> => {
     const { prisoner } = req
     const { token } = res.locals.user
-    const [nextCourtEvent, adjustments, adaIntercept] = await Promise.all([
-      this.prisonerService.getNextCourtEvent(prisoner.bookingId as unknown as number, token),
-      this.adjustmentsService.getAdjustments(prisoner.prisonerNumber, token),
-      this.adjustmentsService.getAdaIntercept(prisoner.prisonerNumber, token),
-    ])
+    if (res.locals.user.hasAdjustmentsAccess === true) {
+      const [nextCourtEvent, adjustments, adaIntercept] = await Promise.all([
+        this.prisonerService.getNextCourtEvent(prisoner.bookingId as unknown as number, token),
+        this.adjustmentsService.getAdjustments(prisoner.prisonerNumber, token),
+        this.adjustmentsService.getAdaIntercept(prisoner.prisonerNumber, token),
+      ])
 
-    const aggregatedAdjustments = adjustments
-      .filter(adjustment => !['LAWFULLY_AT_LARGE', 'SPECIAL_REMISSION'].includes(adjustment.adjustmentType))
-      .reduce((previous: { [key: string]: number }, current) => {
-        const total = (previous[current.adjustmentTypeText] ?? 0) + current.days
-        let newAggregate = previous
-        if (total) {
-          // eslint-disable-next-line no-param-reassign
-          newAggregate = ((previous[current.adjustmentTypeText] = total), previous)
-        }
-        return newAggregate
-      }, {})
+      const aggregatedAdjustments = adjustments
+        .filter(adjustment => !['LAWFULLY_AT_LARGE', 'SPECIAL_REMISSION'].includes(adjustment.adjustmentType))
+        .reduce((previous: { [key: string]: number }, current) => {
+          const total = (previous[current.adjustmentTypeText] ?? 0) + current.days
+          let newAggregate = previous
+          if (total) {
+            // eslint-disable-next-line no-param-reassign
+            newAggregate = ((previous[current.adjustmentTypeText] = total), previous)
+          }
+          return newAggregate
+        }, {})
 
-    res.render('pages/prisoner/overview', { prisoner, nextCourtEvent, aggregatedAdjustments, adaIntercept })
+      return res.render('pages/prisoner/overview', { prisoner, nextCourtEvent, aggregatedAdjustments, adaIntercept })
+    }
+    return res.redirect(`${config.calculateReleaseDatesUiUrl}?prisonId=${prisoner.prisonerNumber}`)
   }
 }
