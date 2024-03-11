1eb08c5767aa4794e5ed25c9097226fd79cdb2b8
carloveo-moj <144013256+carloveo-moj@users.noreply.github.com>
2024-03-11 08:24:40 +0000

    CMG-35 add analytics (#41)
    * CMG-35 add analytics
    
    * only init ai if connection string

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -24,6 +24,9 @@ export default function nunjucksSetup(app: express.Express, applicationInfo: App
   app.locals.environmentNameColour = config.environmentName === 'PRE-PRODUCTION' ? 'govuk-tag--green' : ''
   app.locals.digitalPrisonServicesUrl = config.digitalPrisonServicesUrl
   app.locals.adjustmentsUIUrl = config.adjustmentsUIUrl
+  app.locals.appInsightsConnectionString = config.appInsightsConnectionString
+  app.locals.appInsightsApplicationName = applicationInfo.applicationName
+  app.locals.buildNumber = config.buildNumber
 
   if (config.environmentName === 'LOCAL') {
     app.locals.environment = 'local'
