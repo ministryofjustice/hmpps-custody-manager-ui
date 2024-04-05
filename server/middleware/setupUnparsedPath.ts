b8079d0953e4e1071732db2705790b90d2e58db4
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2024-04-05 10:52:37 +0100

    set page title

--- server/middleware/setupUnparsedPath.ts
+++
@@ -1,9 +0,0 @@
-import { RequestHandler } from 'express'
-
-export default function setupFullPath(): RequestHandler {
-  return async (req, res, next) => {
-    const { path } = req.route
-    res.locals.unparsedPath = `${req.baseUrl}${path}`
-    return next()
-  }
-}
