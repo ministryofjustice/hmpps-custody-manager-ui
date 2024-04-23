75a2b7b3baef6ca48162accdc40fedc748e286b5
yasin <yasin.mustafa@gmail.com>
2024-04-23 17:36:24 +0100

    fix int tests

--- integration_tests/mockApis/prisonApi.ts
+++ integration_tests/mockApis/prisonApi.ts
@@ -69,4 +69,67 @@ export default {
         ],
       },
     }),
+
+  stubGetSentencesAndOffences: (): SuperAgentRequest => {
+    return stubFor({
+      request: {
+        method: 'GET',
+        urlPattern: '/prison-api/api/offender-sentences/booking/1234/sentences-and-offences',
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: [
+          {
+            terms: [
+              {
+                years: 3,
+              },
+            ],
+            sentenceCalculationType: 'ADIMP',
+            sentenceTypeDescription: 'SDS Standard Sentence',
+            caseSequence: 1,
+            lineSequence: 1,
+            caseReference: 'ABC123',
+            sentenceSequence: 1,
+            sentenceStatus: 'A',
+            sentenceDate: '2001-01-01',
+            courtDescription: 'Whiterun Hall of Justice',
+            offences: [
+              {
+                offenceEndDate: '2000-02-03',
+                offenceCode: 'abc',
+                offenderChargeId: 111,
+                offenceDescription: 'Doing a crime',
+              },
+            ],
+          },
+          {
+            terms: [
+              {
+                years: 3,
+              },
+            ],
+            sentenceCalculationType: 'ADIMP',
+            sentenceTypeDescription: 'SDS Standard Sentence',
+            caseSequence: 2,
+            lineSequence: 2,
+            caseReference: 'ABC123',
+            sentenceSequence: 2,
+            sentenceStatus: 'A',
+            sentenceDate: '2001-02-01',
+            courtDescription: 'Whiterun Hall of Justice',
+            offences: [
+              {
+                offenceEndDate: '2000-02-03',
+                offenceCode: 'abc',
+                offenderChargeId: 111,
+                offenceDescription: 'Doing a crime',
+              },
+            ],
+          },
+        ],
+      },
+    })
+  },
 }
