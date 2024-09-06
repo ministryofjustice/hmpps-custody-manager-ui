882234955bd2e076dba45cdf223a99aa2eef9861
yasin <yasin.mustafa@gmail.com>
2024-09-06 13:39:26 +0100

    CMG-171 Hide adjustments when the pridoner is OUT or TRN

--- server/routes/prisoner/handlers/overview.ts
+++ server/routes/prisoner/handlers/overview.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express'
-import { LatestCalculationCardConfig } from 'hmpps-court-cases-release-dates-design/hmpps/@types'
 import PrisonerService from '../../../services/prisonerService'
 import AdjustmentsService from '../../../services/adjustmentsService'
 import config from '../../../config'
 import CalculateReleaseDatesService from '../../../services/calculateReleaseDatesService'
+import { Prisoner } from '../../../@types/prisonerSearchApi/types'
 
 export default class OverviewRoutes {
   constructor(
@@ -14,17 +14,26 @@ export default class OverviewRoutes {
 
   GET = async (req: Request, res: Response): Promise<void> => {
     const { prisoner } = req
-    const { token, activeCaseLoadId, username } = res.locals.user
+    const { token, activeCaseLoadId, username, hasInactiveBookingAccess } = res.locals.user
     const bookingId = prisoner.bookingId as unknown as number
+    const isPrisonerOutside = prisoner.prisonId === 'OUT'
+    const isPrisonerBeingTransferred = prisoner.prisonId === 'TRN'
+    const showAdjustments = !(hasInactiveBookingAccess && (isPrisonerOutside || isPrisonerBeingTransferred))
 
     if (res.locals.user.hasAdjustmentsAccess === true) {
-      const getStartOfSentenceEnvelope = await this.prisonerService.getStartOfSentenceEnvelope(bookingId, token)
-      const [nextCourtEvent, adjustments, adaIntercept, hasActiveSentences] = await Promise.all([
+      const startOfSentenceEnvelope = await this.prisonerService.getStartOfSentenceEnvelope(bookingId, token)
+      const [nextCourtEvent, hasActiveSentences] = await Promise.all([
         this.prisonerService.getNextCourtEvent(bookingId, token),
-        this.adjustmentsService.getAdjustments(prisoner.prisonerNumber, getStartOfSentenceEnvelope, username),
-        this.adjustmentsService.getAdaIntercept(prisoner.prisonerNumber, activeCaseLoadId, username),
         this.prisonerService.hasActiveSentences(bookingId, token),
       ])
+
+      const adaIntercept = showAdjustments
+        ? await this.adjustmentsService.getAdaIntercept(prisoner.prisonerNumber, activeCaseLoadId, username)
+        : null
+
+      const aggregatedAdjustments = showAdjustments
+        ? await this.getAggratedAdjustments(prisoner, startOfSentenceEnvelope, username)
+        : null
 
       const latestCalculationConfig = hasActiveSentences
         ? await this.calculateReleaseDatesService.getLatestCalculationForPrisoner(prisoner.prisonerNumber, token)
@@ -35,22 +44,6 @@ export default class OverviewRoutes {
           ? await this.calculateReleaseDatesService.hasIndeterminateSentences(bookingId, token)
           : false
 
-      const aggregatedAdjustments = adjustments
-        .filter(adjustment => !['LAWFULLY_AT_LARGE', 'SPECIAL_REMISSION'].includes(adjustment.adjustmentType))
-        .reduce(
-          (previous: { [arithmeticKey: string]: { [typeKey: string]: number } }, current) => {
-            const total = (previous[current.adjustmentArithmeticType][current.adjustmentTypeText] ?? 0) + current.days
-            let newAggregate = previous
-            if (total) {
-              newAggregate =
-                // eslint-disable-next-line no-param-reassign
-                ((previous[current.adjustmentArithmeticType][current.adjustmentTypeText] = total), previous)
-            }
-            return newAggregate
-          },
-          { ADDITION: {}, DEDUCTION: {}, NONE: {} },
-        )
-
       return res.render('pages/prisoner/overview', {
         prisoner,
         nextCourtEvent,
@@ -59,17 +52,33 @@ export default class OverviewRoutes {
         latestCalculationConfig,
         isIndeterminateAndHasNoCalculatedDates,
         hasActiveSentences,
+        showAdjustments,
       })
     }
     return res.redirect(`${config.calculateReleaseDatesUiUrl}?prisonId=${prisoner.prisonerNumber}`)
   }
 
-  private datesDontExistInCalc(latestCalculationConfig: LatestCalculationCardConfig): boolean {
-    return (
-      latestCalculationConfig === undefined ||
-      latestCalculationConfig.dates === null ||
-      latestCalculationConfig.dates === undefined ||
-      latestCalculationConfig.dates.length === 0
+  private async getAggratedAdjustments(prisoner: Prisoner, startOfSentenceEnvelope: Date, username: string) {
+    const adjustments = await this.adjustmentsService.getAdjustments(
+      prisoner.prisonerNumber,
+      startOfSentenceEnvelope,
+      username,
     )
+
+    return adjustments
+      .filter(adjustment => !['LAWFULLY_AT_LARGE', 'SPECIAL_REMISSION'].includes(adjustment.adjustmentType))
+      .reduce(
+        (previous: { [arithmeticKey: string]: { [typeKey: string]: number } }, current) => {
+          const total = (previous[current.adjustmentArithmeticType][current.adjustmentTypeText] ?? 0) + current.days
+          let newAggregate = previous
+          if (total) {
+            newAggregate =
+              // eslint-disable-next-line no-param-reassign
+              ((previous[current.adjustmentArithmeticType][current.adjustmentTypeText] = total), previous)
+          }
+          return newAggregate
+        },
+        { ADDITION: {}, DEDUCTION: {}, NONE: {} },
+      )
   }
 }
